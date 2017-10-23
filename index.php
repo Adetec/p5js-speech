e0f74c132dfac11828256aff43f3980bf5e7b9b2
trumpetYK <lim0386@gmail.com>
2017-10-23 16:38:37 +0900

    erase Backgound
    OK

--- index.php
+++ index.php
@@ -17,10 +17,10 @@
 		// createCanvas(800, 400);
 		createCanvas(2048, 1080);
 		background(255, 255, 255);
-		// fill(0, 0, 0, 255);
+		fill(0, 0, 0, 255);
 		// instructions:
-		// textSize(32);
-		// textAlign(CENTER);
+		textSize(32);
+		textAlign(CENTER);
 		// text("say something", width/2, height/2);
 		// myRec.onResult = showResult;
 		// myRec.start();
@@ -36,9 +36,7 @@
 	function showResult()
 	{
 		if(myRec.resultValue==true) {
-			background(192, 255, 192);
-			textSize(32);
-			textAlign(CENTER);
+			// background(192, 255, 192);
 			text(myRec.resultString, width/2, height/2);
 			console.log(myRec.resultString);
 		}
