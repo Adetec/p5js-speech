95034d20f90ba7fc3459fbc356d87ac0ca9a5830
lim0386 <lim0386@gmail.com>
2018-03-09 01:43:43 +0900

    test

--- index.php
+++ index.php
@@ -38,7 +38,8 @@
 	{
 		if(myRec.resultValue==true) {
 			// background(192, 255, 192);
-			text(myRec.resultString, width/2, height/2);
+// 			text(myRec.resultString, width/2, height/2);
+			text(myRec, width/2, height/2);
 			console.log(myRec.resultString);
 		}
 	}
